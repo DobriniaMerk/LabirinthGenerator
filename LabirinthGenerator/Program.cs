aec70004709a22db390c7cc9990da236efa24247
MerkD <dobrinia.merk@yandex.ru>
2024-06-26 23:09:33 +0300

    Added keys to switch grid lines and restart

--- LabirinthGenerator/Program.cs
+++ LabirinthGenerator/Program.cs
@@ -9,8 +9,12 @@ VideoMode vm = new VideoMode(800, 800);
 RenderWindow rw = new RenderWindow(vm, "Labirinth", Styles.Close, new ContextSettings(32, 32, 8));
 Labirinth l = new Labirinth(30, 30);
 
-rw.Closed += OnClose;
 bool done = false;
+bool grid = false;
+
+rw.KeyPressed += OnKey;
+rw.Closed += OnClose;
+
 
 while (rw.IsOpen)
 {
@@ -18,11 +22,22 @@ while (rw.IsOpen)
     rw.Clear();
     if(!done)
         done = l.GenerateStep();
-    l.Draw(rw);
+    l.Draw(rw, grid);
     rw.Display();
 }
 
-
+void OnKey(object sender, KeyEventArgs e)
+{
+    switch (e.Code)
+    {
+        case Keyboard.Key.G:
+            grid = !grid;
+            break;
+        case Keyboard.Key.R:
+            l = new Labirinth(l.width, l.height);
+            break;
+    }
+}
 
 static void OnClose(object sender, EventArgs e)
 {
